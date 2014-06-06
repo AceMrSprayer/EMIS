cf04edc728314a8c1f39608e113823e2b521b58b
kilian <k.kuiper89@gmail.com>
2014-06-06 10:53:06 +0200

    Create functions for stored procedures dynamic calls

---
+++ functions/function.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Neophema
+ * Date: 5-6-14
+ * Time: 15:29
+ */
+
+namespace functions;
+
+
+class functions {
+    function callStoredProcedure() {
+        echo "blaat";
+    }
+} 
---
+++ troubleshoot.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Neophema
+ * Date: 5-6-14
+ * Time: 15:10
+ */ 
