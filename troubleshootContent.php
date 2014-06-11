0b3999b3d161d978b0162cb3caeb93795f82caaa
HansKlokkenspel <n.ulrich1995@gmail.com>
2014-06-11 10:17:12 +0200

    troubleshoot content

--- troubleshootContent.php
+++ troubleshootContent.php
@@ -9,5 +9,55 @@ $functions = new functions();
 ?>
 
 <legend>
-    <b><?php echo $_POST['procName']; ?></b>
+    	 <?php
+    		$procName = $_POST['procName']; 
+        	$obj = $functions->callStoredProcedure("procGetParameters", "procCheckOperators", NULL, NULL, NULL, NULL);
+        ?>
+        	<table class='table table-hover' style='cursor: pointer;'>
+            <?php
+	            for($i = 0; $i < count($obj); $i++) {
+	            	$value = $obj[$i]->PARAMETER_NAME;
+	            	echo '
+	            	<thead>	
+	            		<tr>';
+	            			printf('<th>\'%s\'</th>', $value, $value);
+	            	echo'
+	            		</tr>
+	            	</thead>';
+
+	                
+	                if(strpos($value,'tabel') !== false){
+	                	printf('<tr><td onClick="makeRequest(\'%s\');">%s</td></tr> ', $value, $value);
+	                }
+
+	                if(strpos($value,'kolom') !== false){
+	                	printf('<tr><td onClick="makeRequest(\'%s\');">%s</td></tr> ', $value, $value);
+	                }
+	                if(strpos($value,'operator') !== false){
+	                	printf('<tr><td onClick="makeRequest(\'%s\');">%s</td></tr> ', $value, $value);
+	                }
+	                
+	            }
+          	?>
+
+          	<table class='table table-hover' style='width: 300px; cursor: pointer;float: right;'>
+            <?php
+	            for($i = 0; $i < count($obj); $i++) {
+	                $value = $obj[$i]->PARAMETER_NAME;
+	                if(strpos($value,'tabel') !== false){
+	                	printf('<tr><td onClick="makeRequest(\'%s\');">%s</td></tr> ', $value, $value);
+	                	
+	                }
+
+	                if(strpos($value,'kolom') !== false){
+	                	printf('<tr><td onClick="makeRequest(\'%s\');">%s</td></tr> ', $value, $value);
+	                }
+	                if(strpos($value,'operator') !== false){
+	                	printf('<tr><td onClick="makeRequest(\'%s\');">%s</td></tr> ', $value, $value);
+	                }
+	                
+	            }
+          	?>
+    	
 </legend>
+
